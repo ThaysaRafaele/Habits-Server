9404e2e4abadee7e2fe9a725f03a74c6ed427e29
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-01-22 22:32:18 -0400

    finalização aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,14 @@
-// Backend API restful
-//fastify é um framework semelhante ao express
-
 import Fastify from 'fastify'
-import {PrismaClient} from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from "./route"
 
+const app = Fastify()
 
- //criando a aplicação
- const app = Fastify()
- const prisma = new PrismaClient()
+app.register(cors)
+app.register(appRoutes)
 
- app.register(cors)
-
-/*
-    Método HTTP: 
-    Get => sempre que for buscar informação, 
-    Post => uma rota que vai criar alguma coisa, 
-    Put => rota para atualizar algum recurso por completo, 
-    Patch => quando vai atualizar uma informação específica de um recurso, 
-    Delete => quando vou deletar um recurso dentro do backend
-*/
-
-//criando a rota
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-
-    return habits
-})
-
-//escutando a porta
 app.listen({
-    port: 3333,
+  port: 3333,
 }).then(() => {
-    console.log('HTTP Server running!')
+  console.log('HTTP Server running!')
 })
-
-
-
-
-
